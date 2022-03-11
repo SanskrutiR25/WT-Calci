33ff4f26477b3102ea97dd3735192df73b2ee3d9
Sanskruti Raskar <91798772+SanskrutiR25@users.noreply.github.com>
2022-03-11 13:12:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function myfunction() {
+        let input_value = document.getElementById("display");
+        try{
+            const new_str = eval(input_value.value);
+            input_value.value = new_str;
+            }
+            
+        catch{
+            alert("Enter valid value");
+            input_value.value = "";
+            }
+            
+            }
+
